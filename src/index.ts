94a4d4a6ed55ffe2b797c3db78cc73f4e52a16b3
esnode85 <esnodejs@gmail.com>
2020-03-11 08:40:51 +0200

    fix and update issues

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ console.log("hello I am  node app from typescript");
 console.log("hello I am  node app from typescript");
 
 class Name {
-  private name: string;
-  constructor(name: string) {
+  // private name: string;
+  constructor(private name: string) {
     this.name = name;
     console.log("my name is ==> ", this.name);
   }
@@ -14,7 +14,14 @@ class Name {
     this.name = name;
     console.log("my name is ==> ", this.name);
   }
+  /**
+   * getName
+   */
+  public getName(): string {
+    return this.name;
+  }
 }
 
 const myName = new Name(`eslam`);
+console.log(myName.getName());
 myName.setName("farida");
